9f596db4be830cffb659d424d8f186d6ca2a8b88
shoby <shoby@pixiv.com>
2015-01-07 00:32:43 +0900

    Initial Commit

---
+++ CatHunt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CatHunt
+//
+//  Created by shoby on 2015/01/07.
+//  Copyright (c) 2015年 shoby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
